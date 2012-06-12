f685b5f3f918ecf27c4ef3d717d430ff2e254d32
Niu Yi <niuyi2010@gmail.com>
2012-06-12 13:05:20 +0800

    first add

---
+++ src/com/niuyi/test/PersonalActivity.java
@@ -0,0 +1,7 @@
+package com.niuyi.test;
+
+import android.app.Activity;
+
+public class PersonalActivity extends Activity {
+
+}
---
+++ src/com/niuyi/test/ZhiBoActivity.java
@@ -0,0 +1,7 @@
+package com.niuyi.test;
+
+import android.app.Activity;
+
+public class ZhiBoActivity extends Activity {
+
+}
