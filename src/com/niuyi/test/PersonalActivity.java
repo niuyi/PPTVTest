9afec2fa04d9dceeb9336f7865486a1f9fda0637
Niu Yi <niuyi2010@gmail.com>
2012-06-12 13:48:00 +0800

    add tab

--- src/com/niuyi/test/PersonalActivity.java
+++ src/com/niuyi/test/PersonalActivity.java
@@ -1,7 +1,18 @@
 package com.niuyi.test;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class PersonalActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+		TextView view = new TextView(this);
+		view.setText("Personal");
+		
+		setContentView(view);
+	}
 
 }
--- src/com/niuyi/test/PinDaoActivity.java
+++ src/com/niuyi/test/PinDaoActivity.java
@@ -1,7 +1,19 @@
 package com.niuyi.test;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class PinDaoActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+		TextView view = new TextView(this);
+		view.setText("PinDao");
+		
+		this.setContentView(view);
+	}
 
 }
